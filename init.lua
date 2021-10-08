4ed92295449ce9f81737689f5552e61162bf7e67
Sai Ashirwad <saiashirwadr@gmail.com>
2021-10-09 01:41:01 +0530

    sigh

--- init.lua
+++ init.lua
@@ -452,14 +452,4 @@ require("indent_blankline").setup {
     buftype_exclude = {"terminal"}
 }
 
-
--- Example config in Lua
-vim.g.tokyonight_style = "night"
-vim.g.tokyonight_italic_functions = true
-vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
-
--- Change the "hint" color to the "orange" color, and make the "error" color bright red
-vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
-
--- Load the colorscheme
 vim.cmd[[colorscheme moonlight]]
