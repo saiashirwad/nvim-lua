dd5a3d0a66d70af0de27608bf204dccab2ad971f
Sai Ashirwad <saiashirwadr@gmail.com>
2021-10-09 00:28:21 +0530

    eh

---
+++ init.lua
@@ -0,0 +1,398 @@
+local cmd = vim.cmd
+local fn = vim.fn
+local g = vim.g
+local opt = vim.opt
+
+g.mapleader = " "
+
+vim.cmd [[packadd packer.nvim]]
+require('packer').startup(function(use)
+	use 'wbthomason/packer.nvim'
+  use {
+    "folke/twilight.nvim",
+    config = function()
+      require("twilight").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
+  use 'jiangmiao/auto-pairs'
+	use 'Iron-E/nvim-highlite'
+	use 'szw/vim-maximizer'
+	use 'kassio/neoterm'
+	use 'sbdchd/neoformat'
+	use 'neovim/nvim-lspconfig'
+  use 'preservim/nerdcommenter'
+	use 'hrsh7th/nvim-cmp'
+	use 'hrsh7th/cmp-buffer'
+  use {
+      'kyazdani42/nvim-tree.lua',
+      requires = 'kyazdani42/nvim-web-devicons',
+      config = function() require'nvim-tree'.setup {} end
+  }
+	use 'hrsh7th/cmp-nvim-lua'
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'vimwiki/vimwiki'
+	use { 'nvim-treesitter/nvim-treesitter', branch = '0.5-compat', run = ':TSUpdate' }
+	use 'nvim-telescope/telescope.nvim'
+	use 'nvim-lua/plenary.nvim'
+	use 'nvim-lua/popup.nvim'
+	use 'alaviss/nim.nvim'
+	use 'lewis6991/gitsigns.nvim'
+  use "rafamadriz/neon"
+	use 'mfussenegger/nvim-dap'
+	use 'nvim-telescope/telescope-dap.nvim'
+	use 'theHamsta/nvim-dap-virtual-text'
+	use 'Mofiqul/vscode.nvim'
+	use 'hoob3rt/lualine.nvim'
+	use 'kyazdani42/nvim-web-devicons'
+	use 'ryanoasis/vim-devicons'
+	use 'TimUntersberger/neogit'
+	use 'projekt0n/github-nvim-theme'
+	use 'David-Kunz/jester'
+	use 'vhyrro/neorg'
+	use 'folke/zen-mode.nvim'
+	use 'nvim-treesitter/playground'
+	use 'David-Kunz/treesitter-unit'
+	-- use 'ahmedkhalf/project.nvim'
+	-- use 'tamago324/lir.nvim'
+	-- use 'kdheepak/lazygit.nvim'
+	use 'Pocco81/TrueZen.nvim'
+	use 'tpope/vim-fugitive'
+	use 'p00f/nvim-ts-rainbow'
+	use 'sindrets/diffview.nvim'
+end)
+
+opt.completeopt = {'menu', 'menuone', 'noselect'}
+opt.clipboard = 'unnamedplus'
+opt.mouse = 'a'
+opt.splitright = true
+opt.splitbelow = true
+opt.expandtab = true
+opt.wrap = false
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.hlsearch = false
+opt.number = true
+opt.relativenumber = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.incsearch = true
+-- set diffopt+=vertical " starts diff mode in vertical split
+opt.hidden = true
+opt.cmdheight = 1
+-- set shortmess+=c " don't need to press enter so often
+opt.signcolumn = 'yes'
+opt.updatetime = 520
+opt.undofile = true
+cmd('filetype plugin indent on')
+opt.backup = false
+g.netrw_banner = false
+g.netrw_liststyle = 3
+g.markdown_fenced_languages = { 'javascript', 'js=javascript', 'json=javascript' }
+
+
+local function map(mode, lhs, rhs, opts)
+  local options = {noremap = true}
+  if opts then options = vim.tbl_extend('force', options, opts) end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+map('n', '<leader>v', ':e $MYVIMRC<CR>')
+
+require('gitsigns').setup({})
+
+vim.g.neon_style = "doom" vim.g.neon_italic_keyword = true
+vim.g.neon_italic_function = true
+vim.g.neon_transparent = true
+
+g.neoterm_default_mod = 'botright'
+g.neoterm_autoinsert = true
+g.neoterm_autoscroll = true
+g.neoterm_term_per_tab = true
+map('n', '<c-y>', ':Ttoggle<CR>')
+map('i', '<c-y>', '<Esc>:Ttoggle<CR>')
+map('t', '<c-y>', '<c-\\><c-n>:Ttoggle<CR>')
+map('n', '<leader>x', ':TREPLSendLine<CR>')
+map('v', '<leader>x', ':TREPLSendSelection<CR>')
+cmd([[
+if has('nvim')
+   au! TermOpen * tnoremap <buffer> <Esc> <c-\><c-n>
+endif]])
+
+require('lualine').setup({
+  options = {
+    theme = "vscode",
+    component_separators = {'', ''},
+    section_separators = {'', ''},
+  },
+  sections = {
+    lualine_a = {{'filename', path = 2}},
+    lualine_b = {'branch', {
+      'diff',
+      color_added = 'green',
+      color_modified = 'yellow',
+      color_removed = 'red'
+    }},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {}
+  },
+})
+
+-- sbdchd/neoformat
+map('n', '<leader>F', ':Neoformat prettier<CR>')
+
+map('', '<C-w>m', ':MaximizerToggle!<CR>')
+
+-- nvim-telescope/telescope.nvim
+_G.telescope_find_files_in_path = function(path)
+ local _path = path or vim.fn.input("Dir: ", "", "dir")
+ require("telescope.builtin").find_files({search_dirs = {_path}})
+end
+_G.telescope_live_grep_in_path = function(path)
+ local _path = path or vim.fn.input("Dir: ", "", "dir")
+ require("telescope.builtin").live_grep({search_dirs = {_path}})
+end
+_G.telescope_files_or_git_files = function()
+ local utils = require('telescope.utils')
+ local builtin = require('telescope.builtin')
+ local _, ret, _ = utils.get_os_command_output({ 'git', 'rev-parse', '--is-inside-work-tree' })
+ if ret == 0 then
+   builtin.git_files()
+ else
+   builtin.find_files()
+ end
+end
+map('n', '<leader>fD', ':lua telescope_live_grep_in_path()<CR>')
+map('n', '<leader><space>', ':lua telescope_files_or_git_files()<CR>')
+map('n', '<leader>fd', ':lua telescope_find_files_in_path()<CR>')
+map('n', '<leader>ft', ':lua telescope_find_files_in_path("./tests")<CR>')
+map('n', '<leader>fT', ':lua telescope_live_grep_in_path("./tests")<CR>')
+map('n', '<leader>ff', ':Telescope live_grep<CR>')
+map('n', '<leader>fo', ':Telescope file_browser<CR>')
+map('n', '<leader>fn', ':Telescope find_files<CR>')
+map('n', '<leader>fg', ':Telescope git_branches<CR>')
+map('n', '<leader>fb', ':Telescope buffers<CR>')
+map('n', '<leader>fs', ':Telescope lsp_document_symbols<CR>')
+map('n', '<leader>ff', ':Telescope live_grep<CR>')
+map('n', '<leader>FF', ':Telescope grep_string<CR>')
+
+
+-- Lsp
+local nvim_lsp = require'lspconfig'
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  local opts = { noremap=true, silent=true }
+
+  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+
+end
+
+local servers = { 'pyright', 'rust_analyzer', 'tsserver' }
+for _, lsp in ipairs(servers) do
+  nvim_lsp[lsp].setup {
+    on_attach = on_attach,
+    flags = {
+      debounce_text_changes = 150,
+    }
+  }
+end
+
+-- -- neovim/nvim-lspconfig
+-- local nvim_lsp = require'lspconfig'
+-- local on_attach = function(client, bufnr)
+--     local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+--     buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+-- end
+-- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities(), {
+--   snippetSupport = false,
+-- })
+-- nvim_lsp.tsserver.setup{ capabilities = capabilities }
+-- local servers = { 'tsserver' }
+-- for _, lsp in ipairs(servers) do
+--   nvim_lsp[lsp].setup {
+--     on_attach = on_attach
+--   }
+-- end
+
+-- map('n', 'gd', ':lua vim.lsp.buf.definition()<CR>')
+-- map('n', 'gh', ':lua vim.lsp.buf.hover()<CR>')
+-- map('n', 'ga', ':Telescope lsp_code_actions<CR>')
+-- map('n', 'gA', ':Telescope lsp_range_code_actions<CR>')
+-- map('n', 'gD', ':lua vim.lsp.buf.implementation()<CR>')
+-- map('n', 'K', ':lua vim.lsp.buf.signature_help()<CR>')
+-- map('n', 'gr', ':lua vim.lsp.buf.references()<CR>')
+-- map('n', 'gR', ':lua vim.lsp.buf.rename()<CR>')
+ 
+-- nvim/treesitter
+g.vscode_style = "dark"
+cmd('colorscheme vscode')
+cmd('set foldmethod=expr')
+cmd('set foldexpr=nvim_treesitter#foldexpr()')
+
+map('n', '<leader>nn', ':tabe ~/tmp/notes.md<CR>')
+
+require'nvim-treesitter.configs'.setup {
+  highlight = {
+    enable = true,
+  },
+  indent = {
+    enable = true
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true,
+      keymaps = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+        ["ab"] = "@block.outer",
+        ["ib"] = "@block.inner"
+      }
+    }
+  }
+}
+
+-- -- TimUntersberger/neogit and sindrets/diffview.nvim
+require'diffview'.setup {
+  file_panel = {
+    position = "left",            -- One of 'left', 'right', 'top', 'bottom'
+    width = 60,                   -- Only applies when position is 'left' or 'right'
+  }
+}
+
+require("neogit").setup {
+  disable_commit_confirmation = true,
+  integrations = {
+    diffview = true
+    }
+  }
+map('n', '<leader>gg', ':Neogit<cr>')
+map('n', '<leader>gd', ':DiffviewOpen<cr>')
+map('n', '<leader>gD', ':DiffviewOpen main<cr>')
+map('n', '<leader>gl', ':Neogit log<cr>')
+map('n', '<leader>gp', ':Neogit push<cr>')
+
+-- 'tpope/vim-fugitive'
+map('n', '<leader>gg', ':Git<cr>')
+map('n', '<leader>gd', ':tabe %<cr>:Gvdiffsplit!<CR>')
+map('n', '<leader>gD', ':DiffviewOpen<cr>')
+map('n', '<leader>gm', ':tabe %<cr>:Gvdiffsplit! main<CR>')
+map('n', '<leader>gM', ':DiffviewOpen main<cr>')
+map('n', '<leader>gl', ':Git log<cr>')
+map('n', '<leader>gp', ':Git push<cr>')
+
+
+-- hrsh7th/nvim-cmp
+local cmp = require('cmp')
+  cmp.setup {
+    mapping = {
+      ['<C-p>'] = cmp.mapping.select_prev_item(),
+      ['<C-n>'] = cmp.mapping.select_next_item(),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.close(),
+      ['<CR>'] = cmp.mapping.confirm({
+        behavior = cmp.ConfirmBehavior.Insert,
+      })
+    },
+    sources = {
+      { name = 'nvim_lsp' },
+      { name = 'buffer' },
+    },
+  }
+
+local au = require('au')
+
+
+-- following options are the default
+require'nvim-tree'.setup {
+  -- disables netrw completely
+  disable_netrw       = true,
+  -- hijack netrw window on startup
+  hijack_netrw        = true,
+  -- open the tree when running this setup function
+  open_on_setup       = false,
+  -- will not open on setup if the filetype is in this list
+  ignore_ft_on_setup  = {},
+  -- closes neovim automatically when the tree is the last **WINDOW** in the view
+  auto_close          = false,
+  -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
+  open_on_tab         = false,
+  -- hijacks new directory buffers when they are opened.
+  update_to_buf_dir   = {
+    -- enable the feature
+    enable = true,
+    -- allow to open the tree if it was previously closed
+    auto_open = true,
+  },
+  -- hijack the cursor in the tree to put it at the start of the filename
+  hijack_cursor       = false,
+  -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
+  update_cwd          = false,
+  -- show lsp diagnostics in the signcolumn
+  lsp_diagnostics     = false,
+  -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
+  update_focused_file = {
+    -- enables the feature
+    enable      = false,
+    -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
+    -- only relevant when `update_focused_file.enable` is true
+    update_cwd  = false,
+    -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
+    -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
+    ignore_list = {}
+  },
+  -- configuration options for the system open command (`s` in the tree by default)
+  system_open = {
+    -- the command to run this, leaving nil should work in most cases
+    cmd  = nil,
+    -- the command arguments as a list
+    args = {}
+  },
+
+  view = {
+    -- width of the window, can be either a number (columns) or a string in `%`, for left or right side placement
+    width = 30,
+    -- height of the window, can be either a number (columns) or a string in `%`, for top or bottom side placement
+    height = 30,
+    -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
+    side = 'left',
+    -- if true the tree will resize itself after opening a file
+    auto_resize = false,
+    mappings = {
+      -- custom only false will merge the list with the default mappings
+      -- if true, it will only use your list to set the mappings
+      custom_only = false,
+      -- list of mappings to set on the tree manually
+      list = {}
+    }
+  }
+}
+
+
---
+++ plugin/packer_compiled.lua
@@ -0,0 +1,250 @@
+-- Automatically generated packer.nvim plugin loader code
+
+if vim.api.nvim_call_function('has', {'nvim-0.5'}) ~= 1 then
+  vim.api.nvim_command('echohl WarningMsg | echom "Invalid Neovim version for packer.nvim! | echohl None"')
+  return
+end
+
+vim.api.nvim_command('packadd packer.nvim')
+
+local no_errors, error_msg = pcall(function()
+
+  local time
+  local profile_info
+  local should_profile = false
+  if should_profile then
+    local hrtime = vim.loop.hrtime
+    profile_info = {}
+    time = function(chunk, start)
+      if start then
+        profile_info[chunk] = hrtime()
+      else
+        profile_info[chunk] = (hrtime() - profile_info[chunk]) / 1e6
+      end
+    end
+  else
+    time = function(chunk, start) end
+  end
+  
+local function save_profiles(threshold)
+  local sorted_times = {}
+  for chunk_name, time_taken in pairs(profile_info) do
+    sorted_times[#sorted_times + 1] = {chunk_name, time_taken}
+  end
+  table.sort(sorted_times, function(a, b) return a[2] > b[2] end)
+  local results = {}
+  for i, elem in ipairs(sorted_times) do
+    if not threshold or threshold and elem[2] > threshold then
+      results[i] = elem[1] .. ' took ' .. elem[2] .. 'ms'
+    end
+  end
+
+  _G._packer = _G._packer or {}
+  _G._packer.profile_output = results
+end
+
+time([[Luarocks path setup]], true)
+local package_path_str = "/home/sai/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/sai/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/sai/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/sai/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/sai/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+if not string.find(package.path, package_path_str, 1, true) then
+  package.path = package.path .. ';' .. package_path_str
+end
+
+if not string.find(package.cpath, install_cpath_pattern, 1, true) then
+  package.cpath = package.cpath .. ';' .. install_cpath_pattern
+end
+
+time([[Luarocks path setup]], false)
+time([[try_loadstring definition]], true)
+local function try_loadstring(s, component, name)
+  local success, result = pcall(loadstring(s))
+  if not success then
+    vim.schedule(function()
+      vim.api.nvim_notify('packer.nvim: Error running ' .. component .. ' for ' .. name .. ': ' .. result, vim.log.levels.ERROR, {})
+    end)
+  end
+  return result
+end
+
+time([[try_loadstring definition]], false)
+time([[Defining packer_plugins]], true)
+_G.packer_plugins = {
+  ["TrueZen.nvim"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/TrueZen.nvim"
+  },
+  ["auto-pairs"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/auto-pairs"
+  },
+  ["cmp-buffer"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/cmp-buffer"
+  },
+  ["cmp-nvim-lsp"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp"
+  },
+  ["cmp-nvim-lua"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/cmp-nvim-lua"
+  },
+  ["diffview.nvim"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/diffview.nvim"
+  },
+  ["github-nvim-theme"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/github-nvim-theme"
+  },
+  ["gitsigns.nvim"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/gitsigns.nvim"
+  },
+  jester = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/jester"
+  },
+  ["lualine.nvim"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/lualine.nvim"
+  },
+  neoformat = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/neoformat"
+  },
+  neogit = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/neogit"
+  },
+  neon = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/neon"
+  },
+  neorg = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/neorg"
+  },
+  neoterm = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/neoterm"
+  },
+  nerdcommenter = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/nerdcommenter"
+  },
+  ["nim.nvim"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/nim.nvim"
+  },
+  ["nvim-cmp"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/nvim-cmp"
+  },
+  ["nvim-dap"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/nvim-dap"
+  },
+  ["nvim-dap-virtual-text"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/nvim-dap-virtual-text"
+  },
+  ["nvim-highlite"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/nvim-highlite"
+  },
+  ["nvim-lspconfig"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/nvim-lspconfig"
+  },
+  ["nvim-tree.lua"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/nvim-tree.lua"
+  },
+  ["nvim-treesitter"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/nvim-treesitter"
+  },
+  ["nvim-ts-rainbow"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/nvim-ts-rainbow"
+  },
+  ["nvim-web-devicons"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/nvim-web-devicons"
+  },
+  ["packer.nvim"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/packer.nvim"
+  },
+  playground = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/playground"
+  },
+  ["plenary.nvim"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/plenary.nvim"
+  },
+  ["popup.nvim"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/popup.nvim"
+  },
+  ["telescope-dap.nvim"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/telescope-dap.nvim"
+  },
+  ["telescope.nvim"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/telescope.nvim"
+  },
+  ["treesitter-unit"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/treesitter-unit"
+  },
+  ["twilight.nvim"] = {
+    config = { "\27LJ\2\n:\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\rtwilight\frequire\0" },
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/twilight.nvim"
+  },
+  ["vim-commentary"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/vim-commentary"
+  },
+  ["vim-devicons"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/vim-devicons"
+  },
+  ["vim-fugitive"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/vim-fugitive"
+  },
+  ["vim-maximizer"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/vim-maximizer"
+  },
+  vimwiki = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/vimwiki"
+  },
+  ["vscode.nvim"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/vscode.nvim"
+  },
+  ["zen-mode.nvim"] = {
+    loaded = true,
+    path = "/home/sai/.local/share/nvim/site/pack/packer/start/zen-mode.nvim"
+  }
+}
+
+time([[Defining packer_plugins]], false)
+-- Config for: twilight.nvim
+time([[Config for twilight.nvim]], true)
+try_loadstring("\27LJ\2\n:\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\rtwilight\frequire\0", "config", "twilight.nvim")
+time([[Config for twilight.nvim]], false)
+if should_profile then save_profiles() end
+
+end)
+
+if not no_errors then
+  vim.api.nvim_command('echohl ErrorMsg | echom "Error in packer_compiled: '..error_msg..'" | echom "Please check your config for correctness" | echohl None')
+end
